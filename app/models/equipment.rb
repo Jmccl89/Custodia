31e6b5f03e524c000e20e141ff8daa3c7f3d44c2
James McCloskey <james@mccloskey.io>
2018-04-10 11:06:52 -0600

    clean up rubocop yml to fix lint job failing in ci

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Equipment < ApplicationRecord
-  has_many :entrys
+  has_many :entrys, dependent: :destroy
   validates :name, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 255 }
   validates :serial, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
