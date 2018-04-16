3df5d54b7778876f8b357b5154e458f77761dead
James McCloskey <james@mccloskey.io>
2018-04-16 13:02:59 -0600

    implement entry edit function

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -8,10 +8,26 @@ class EntriesController < ApplicationController
     @equipment = params[:equipment]
     @entry = Equipment.find_by(id: params[:equipment]).entries.build(entry_params)
     if @entry.save
-      flash[:success] = 'Micropost created!'
+      flash[:success] = 'Entry created!'
       redirect_back fallback_location: root_url
     else
       render 'static_pages/home'
+    end
+  end
+
+  def edit
+    @entry = Entry.find_by(params[:id])
+    @equipment = Equipment.find(@entry.equipment_id)
+  end
+
+  def update
+    @entry = Entry.find_by(params[:id])
+    @equipment = Equipment.find(@entry.equipment_id)
+    if @entry.update(entry_params)
+      flash[:success] = 'Entry updated.'
+      redirect_to @equipment
+    else
+      render 'edit'
     end
   end
 
