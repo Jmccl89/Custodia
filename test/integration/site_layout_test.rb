34790a309b3467387eb67e0ab157a751550fc0ee
James McCloskey <james@mccloskey.io>
2018-04-17 10:18:51 -0600

    fix integration test looking for removed signup links

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,6 +8,5 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'static_pages/home'
     assert_select 'a[href=?]', root_path, count: 2
     assert_select 'a[href=?]', login_path
-    assert_select 'a[href=?]', signup_path
   end
 end
