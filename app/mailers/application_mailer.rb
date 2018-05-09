1011e07141d8bfb0a9c87743d84d3b97cd400281
James McCloskey <james@mccloskey.io>
2018-05-09 12:36:27 -0600

    change mailer domain

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'admin@mccloskey.io'
+  default from: 'admin@bpf.farm'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'bpf.mccloskey.io'
+  host = 'custodia.bpf.farm'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
@@ -68,7 +68,7 @@ Rails.application.configure do
     authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'mccloskey.io',
+    domain: 'bpf.farm',
     enable_starttls_auto: true
   }
 
